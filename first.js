d8ef04cd5308e5a778658d87f4da3ad02f470dbb
GitShubhee <vshubhee6520@gmail.com>
2024-05-29 18:22:07 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,28 @@
+console.log("hello shubhee!");
+console.log("i want to do something for you");
+
+fullname = "shubhee";
+console.log(fullname);
+age = 24;
+price = 99.99;
+console.log(age,price);
+//let var  and const
+let fullName = ("shubhee");
+console.log(fullName); var age = 34; var age = 21;
+var age = 12; console.log(age);
+let r;
+
+const student = {
+    fullName :" shubhee",
+    age : 21,
+    cgpa: 8.3,
+    ispass: true,
+
+};
+console.log(student);
+
+
+
+
+
+
